4d482bca03aed347939cf21ffbb5af110d507ccb
mrjoll3y <callen.jolley7@gmail.com>
2024-10-06 19:32:24 -0600

    changes  to the css and fixed some conflicting script

--- week03/scripts/filtered-temples.js
+++ week03/scripts/filtered-temples.js
@@ -99,7 +99,6 @@ const temples = [
     });
   }
   
-  // Event Listeners for Filtering
   document.getElementById("home").addEventListener("click", () => displayTemples(temples));
   document.getElementById("old").addEventListener("click", () => {
     const oldTemples = temples.filter(temple => new Date(temple.dedicated.split(",")[0]).getFullYear() < 1900);
@@ -118,25 +117,9 @@ const temples = [
     displayTemples(smallTemples);
   });
   
-  // Initial Display of All Temples
   displayTemples(temples);
   
-  // Footer Date Update
   document.getElementById("currentyear").textContent = new Date().getFullYear();
   document.getElementById("lastModified").textContent = `Last Modified: ${document.lastModified}`;
 
-const footer = document.querySelector('footer');
-const currentYear = new Date().getFullYear();
-const lastModified = document.lastModified;
-
-footer.innerHTML = `<p>&copy; ${currentYear} Callen Jolley | Last Modified: ${lastModified}</p>`;
-
-const mainnav = document.querySelector("nav");
-const hambutton = document.querySelector("#menu");
-
-hambutton.addEventListener("click", () => {
-  mainnav.classList.toggle("show");
-  hambutton.classList.toggle("show");
-  document.querySelector("h1").classList.toggle("hide");
-});
 
